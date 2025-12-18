574e56e09f1142bdc6eaf78738805ff5ede6b3c9
Sunil Nagar <sunilnagar682724@gmail.com>
2025-12-18 11:39:52 +0530

    handel google login callback

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL:
+      process.env.NODE_ENV === "production"? "https://bhraman-web.onrender.com/auth/google/callback": "http://localhost:8080/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
